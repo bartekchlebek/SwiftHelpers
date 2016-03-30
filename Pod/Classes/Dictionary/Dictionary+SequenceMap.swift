17e04f07b1862d18c895a167f49eb0322465f380
Bartek Chlebek <bartek.public@gmail.com>
2016-03-30 10:48:02 +0200

    Removed SquenceType.dictionaryWithKeyForItem

--- Pod/Classes/Dictionary/Dictionary+SequenceMap.swift
+++ Pod/Classes/Dictionary/Dictionary+SequenceMap.swift
@@ -1,21 +1,11 @@
 extension Dictionary {
-	public init<T where T: SequenceType, T.Generator.Element == Value>
-		(sequence: T, usingKeyForSequenceElement keyForSequenceElement: T.Generator.Element -> Key) {
+	public init<T where T: SequenceType, T.Generator.Element == Value>(sequence: T,
+	            usingKeyForSequenceElement keyForSequenceElement: T.Generator.Element -> Key) {
 		
-		self = sequence.dictionaryWithKeyForItem(keyForSequenceElement)
-	}
-}
-
-extension SequenceType {
-	typealias Element = Generator.Element
-	
-	private func dictionaryWithKeyForItem<U: Hashable>
-		(IDGetterForItem: (Generator.Element) -> U) -> [U: Generator.Element] {
-		
-		var dictionary = Dictionary<U, Element>(minimumCapacity: self.underestimateCount())
-		for item in self {
-			dictionary[IDGetterForItem(item)] = item
+		var dictionary = Dictionary<Key, Value>(minimumCapacity: sequence.underestimateCount())
+		for item in sequence {
+			dictionary[keyForSequenceElement(item)] = item
 		}
-		return dictionary
+		self = dictionary
 	}
 }
